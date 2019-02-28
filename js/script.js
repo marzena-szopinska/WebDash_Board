d88e10045957a6b4e3c44cf27f55c24f222b781b
Marzena <marzena.szopinska090@gmail.com>
2019-02-28 17:50:37 +0100

    added two additional charts

--- js/script.js
+++ js/script.js
@@ -1,5 +1,30 @@
-var ctx = document.getElementById("bar-chart");
-var barChart = new Chart(ctx, {
+const barChart = document.getElementById("bar-chart");
+const doughnutChart = document.getElementById("doughnut-chart");
+const lineChart = document.getElementById("line-chart");
+
+
+let myChart = new Chart(lineChart, {
+    type: 'line',
+    data: {
+        labels: ['16-22', '23-29', '30-5', '6-12', '13-19', '20-26', '27-3', '4-10', '11-17', '18-24', '25-31'],
+        datasets: [
+            {
+                label: 'Visitors',
+                data: [350, 1250, 1000, 2000, 1500, 1750, 1250, 1750, 2250, 1750, 2250],
+                backgroundColor: 'rgba(115, 119, 191, 0.25)',
+                borderColor: '#7477bf',
+                borderWidth: 1.5,
+                pointBorderWidth: 1.8,
+                pointBackgroundColor: '#fff',
+                pointHoverBackgroundColor: '#e7e8f9',
+                pointRadius: 5,
+                lineTension: 0,
+            }
+        ]
+    },
+});
+
+let barTrafficChart = new Chart(barChart, {
     type: 'bar',
     data: {
         labels: ["S", "M", "T", "W", "T", "F", "S"],
@@ -35,5 +60,25 @@ var barChart = new Chart(ctx, {
                 }
             }]
         }
+    }
+});
+
+let doughnutMobileChart = new Chart(doughnutChart, {
+    type: 'doughnut',
+    data: {
+        labels: ['Phones', 'Tablets', 'Desktop'],
+        datasets: [
+            {
+                data: [15, 25, 60],
+                backgroundColor: ['#74b1bf', '#81c98f', '#7377bf'],
+                hoverBackgroundColor: ['#56aabd', '#67c97a', '#6368c0'],
+            }
+        ]
+    },
+    options: {
+        legend: {
+            display: true,
+            position: 'right'
+        },
     }
 });
